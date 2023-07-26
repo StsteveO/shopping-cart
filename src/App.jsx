f5ef4f7830128e583cd8ee30896be62d0210b941
steve omari <steveomari62@gmail.com>
2023-07-26 15:49:49 -0400

    create starting point

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//eslint-disable-next-line
+import { useState, useEffect } from 'react'
+import './App.css'
+
+export default function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='test'>Hello World</div>
+    </>
+  )
+}
