5d61152db461403dca31aa2f304145c75f125dcc
steve omari <steveomari62@gmail.com>
2023-07-27 17:20:42 -0400

    update app.jsx to accept cover page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 //eslint-disable-next-line
 import { useState, useEffect } from 'react'
 import './App.css'
+import CoverPage from './CoverPage'
+
 
 export default function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='test'>Hello World</div>
+      <CoverPage/>
     </>
   )
 }
