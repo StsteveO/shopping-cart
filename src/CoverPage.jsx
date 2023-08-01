b8772e1f65bee8c269929de4619148965642d308
steve omari <steveomari62@gmail.com>
2023-08-01 10:34:16 -0400

    format cover page with credits

--- src/CoverPage.jsx
+++ src/CoverPage.jsx
@@ -8,8 +8,11 @@ export default function CoverPage() {
 
   return (
     <>
-      <div className="test">Cover Page</div>
-      <Link to="mainPageFrame/:">MainFramePage</Link>
+      <div className="cover-page">
+        <div className="cover-page-title">{`Lori's [Lc] Closet`}</div>
+        <Link to="mainPageFrame/:" className="enterStoreSite">Enter Store Site</Link> 
+        <div className="coverPhotoCredit">Photo by <a href="https://unsplash.com/@jorenaranas?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Joren Aranas</a> on <a href="https://unsplash.com/photos/womans-hippie-style-sunglasses-nmuyqgSOpEE?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></div>
+      </div>
     </>
   );
 }
