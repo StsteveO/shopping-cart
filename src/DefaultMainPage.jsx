d5b2c7eeae526636500184e1bb042f8431891c26
steve omari <steveomari62@gmail.com>
2023-08-01 10:36:04 -0400

    format main page

--- src/DefaultMainPage.jsx
+++ src/DefaultMainPage.jsx
@@ -2,18 +2,18 @@
 import { useState, useEffect } from "react";
 //eslint-disable-next-line
 import { Link } from "react-router-dom";
+// import MainPageFrame from "./MainPageFrame";
+import CardsOrganizer from "./CardsOrganizer";
 
-export default function DefaultMainPage() {
+//eslint-disable-next-line
+export default function DefaultMainPage({ fullList, updateCart }) {
   // const [count, setCount] = useState(0)
+  // console.log(fullList);
 
   return (
     <>
-      <div className="test">Default Content</div>
-      <Link to="/">Cover Page</Link>
-      <br />
-      <Link to="/mainPageFrame/mens">Mens Page</Link>
-      <br />
-      <Link to="/mainPageFrame/womens">Womens Page</Link>
+      <h1 className="home-page">All Products</h1>
+      <CardsOrganizer list={fullList} updateCart={updateCart} />
     </>
   );
 }
